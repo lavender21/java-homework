a4748a9d49ca609a5479250bbf21b8f208aed584
rtning <1573872488@qq.com>
2017-08-07 23:48:12 +0800

    add Draw a horizontal method

--- src/JavaPractice.java
+++ src/JavaPractice.java
@@ -1,6 +1,9 @@
 public class JavaPractice {
     public static void main(String[] args) {
         TriangleExercises triangleExercises = new TriangleExercises();
+        System.out.println("Easiest exercise ever\n");
         triangleExercises.easiestExercise();
+        System.out.println("Draw a horizontal line\n");
+        triangleExercises.drawHorizontalLine();
     }
 }
--- src/TriangleExercises.java
+++ src/TriangleExercises.java
@@ -1,5 +1,13 @@
 public class TriangleExercises {
+
     public void easiestExercise() {
         System.out.println('*');
+    }
+
+    public void drawHorizontalLine() {
+        for(int i=0; i<8; i++){
+            System.out.print('*');
+        }
+        System.out.println();
     }
 }
